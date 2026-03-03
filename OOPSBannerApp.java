c6b7ae77b3c79ab038b34368288536cd557241ec
Laasya-AA <lt8302@srmist.edu.in>
2026-03-03 23:23:29 +0530

    Implemented UC4 using String array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,55 +1,64 @@
 /**
  * OOPSBannerApp
- * UC3: Render OOPS using String.join()
+ * UC4: Render OOPS as Banner using String Array and Loop
  * 
  * @author Laasya
- * @version 3.0
+ * @version 4.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ",
+        // Step 1: Create String array
+        String[] lines = new String[7];
+
+        // Step 2: Populate array using String.join()
+        lines[0] = String.join(" ",
                 " ********  ",
                 " ******** ",
                 " ******  ",
-                " ******** "));
+                " ******** ");
 
-        System.out.println(String.join(" ",
+        lines[1] = String.join(" ",
                 "*        * ",
                 "*        * ",
                 "*      * ",
-                "*        "));
+                "*        ");
 
-        System.out.println(String.join(" ",
+        lines[2] = String.join(" ",
                 "*        * ",
                 "*        * ",
                 "*      * ",
-                "*        "));
+                "*        ");
 
-        System.out.println(String.join(" ",
+        lines[3] = String.join(" ",
                 "*        * ",
                 "*        * ",
                 "*******  ",
-                "******** "));
+                "******** ");
 
-        System.out.println(String.join(" ",
+        lines[4] = String.join(" ",
                 "*        * ",
                 "*        * ",
                 "*        ",
-                "       * "));
+                "       * ");
 
-        System.out.println(String.join(" ",
+        lines[5] = String.join(" ",
                 "*        * ",
                 "*        * ",
                 "*       ",
-                "        * "));
+                "        * ");
 
-        System.out.println(String.join(" ",
+        lines[6] = String.join(" ",
                 " ******** ",
                 "  ******** ",
                 " *       ",
-                " ******** "));
+                " ******** ");
+
+        // Step 3: Loop through array
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
